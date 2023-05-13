6477bba67ec6493eea5b773b2f6b922226aac398
Kohei Wakita <kohei_wakita@yahoo.co.jp>
2023-05-13 19:15:59 +0900

    django connect to database

--- backend/quizProject/settings.py
+++ backend/quizProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'quizProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-db',
+        'USER':'django',
+        'PASSWORD':'django',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
