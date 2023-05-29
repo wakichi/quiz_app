848d11ec449b21c43c17f3125543f18d037151b0
Koheiwakita <k.wakita20@gmail.com>
2023-05-29 23:18:38 +0900

    update docker command

--- frontend/web-front/src/index.tsx
+++ frontend/web-front/src/index.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const endpoint = "http://localhost:8000/api/quiz/"
+
+
+export default function App(){
+  const [data, setdata] = React.useState(["str"]);
+  React.useEffect(() => {
+    axios.get(endpoint).then((response)=>{
+      setdata(data2list(response.data))
+      // console.log(typeof(response.data[0]["problem"]))
+    })
+  },[]) 
+  return(
+    <div>
+      <h1>test</h1>
+      <ul>
+        {response2list(data)}
+      </ul>
+    </div>
+  )
+}
+
+const data2list = (data:any)=>{
+  const list =[]
+  for(const d of data){
+    list.push(d["problem"])
+  }
+  return list
+}
+
+const response2list = (data:any)=>{
+  const list = []
+  for (const dt of data){
+    list.push(<li>{dt}</li>)
+  }
+  console.log(list)
+  return(
+    <ul>
+      {list}
+    </ul>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
